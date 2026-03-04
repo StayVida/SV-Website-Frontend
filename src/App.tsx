1224a76fa24ed4d5c234a4b4995773c08e22f75a
SumitMaharana-783 <sumitmaharana@beginning.co.in>
2026-03-04 17:51:55 +0530

    feat: Initialize main application component with routing, authentication context, and toast notifications.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <AppRoutes />
+        <Toaster position="top-center" richColors />
       </BrowserRouter>
     </AuthProvider>
   );
