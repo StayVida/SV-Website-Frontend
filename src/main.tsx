80e997d92c705c5f26db5470d71fe75c846b2163
SumitMaharana-783 <sumitmaharana@beginning.co.in>
2026-04-25 21:15:23 +0530

    feat: integrate React Query provider and Vercel Analytics in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
-
+import { Analytics } from "@vercel/analytics/react"
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 )
