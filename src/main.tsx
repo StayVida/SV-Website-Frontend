cd38619ccc1e21c7a4e043ce0392ea05a8f2365c
SumitMaharana-783 <sumitmaharana@beginning.co.in>
2026-04-25 21:18:15 +0530

    feat: initialize application entry point with Vercel Analytics, Speed Insights, and React Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <Analytics />
+      <SpeedInsights />
     </QueryClientProvider>
   </StrictMode>,
 )
